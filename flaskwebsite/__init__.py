ee20504ff01f552fed85d94b65b8c2ce36182f11
madeleine <yuema0415@gmail.com>
2020-10-01 18:03:37 -0400

    added config

--- flaskwebsite/__init__.py
+++ flaskwebsite/__init__.py
@@ -3,16 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+import config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'bd95b20cb68c470de174216f1ead5b9c'
+app.config['SECRET_KEY'] = config.secret_keys['secret_key']
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
 
 app.config['RECAPTCHA_USE_SSL'] = False
-app.config['RECAPTCHA_PUBLIC_KEY'] = '6Len_c0ZAAAAAIqon7rDn4vCUb_fEqudOInJ_hwO'
-app.config['RECAPTCHA_PRIVATE_KEY'] = '6Len_c0ZAAAAAKCYf6BRGYrUEcU3nF3ExZjxVQdl'
+app.config['RECAPTCHA_PUBLIC_KEY'] = config.secret_keys['recap_public_key']
+app.config['RECAPTCHA_PRIVATE_KEY'] = config.secret_keys['recap_private_key']
 
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
@@ -22,8 +23,8 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = '587'
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'email'
-app.config['MAIL_PASSWORD'] = 'password'
+app.config['MAIL_USERNAME'] = config.email['user']
+app.config['MAIL_PASSWORD'] = config.email['password']
 mail = Mail(app)
 
 
